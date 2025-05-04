a6fc8d9398afa130ad970cd82568e1c1ee9e4929
Caitlyn Zuppinger <caitlyn.zuppinger@swisscom.com>
2025-05-04 21:30:34 +0200

    feat(umap): beginning of umap code

--- config.py
+++ config.py
@@ -7,7 +7,13 @@ DEBUG = os.getenv("DEBUG", "False")
 DATA_PATH = os.getenv("DATA_PATH", "./data")
 OUTPUT_PATH = os.getenv("OUTPUT_PATH", "./outputs")
 
-SCHULTHESS_DATAPATH = os.path.join(DATA_PATH, os.getenv("SCHULTHESS_DATAFOLDER", "images_knee"))
+SCHULTHESS_DATAFOLDER = os.getenv("SCHULTHESS_DATAFOLDER", "images_knee")
+
+if SCHULTHESS_DATAFOLDER == "":
+    SCHULTHESS_DATAPATH = DATA_PATH
+else:
+    SCHULTHESS_DATAPATH = os.path.join(DATA_PATH, SCHULTHESS_DATAFOLDER)
+
 CHENETAL_DATAPATH = os.path.join(DATA_PATH, os.getenv("CHENETAL_DATAFOLDER", "kaggle_dataset"))
 
 PATH_TO_ANOM = os.path.join(OUTPUT_PATH, "dfs")
---
+++ utils/load_utils.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from PIL import Image
+import numpy as np
+
+def load_image_folder_as_array(folder, image_size=(64, 64)):
+    X = []
+    y = []
+    for label_folder in sorted(Path(folder).iterdir()):
+        if label_folder.is_dir():
+            label = label_folder.name
+            for img_path in label_folder.glob("*.jpg"):  # or *.png
+                img = Image.open(img_path).convert("L").resize(image_size)
+                X.append(np.array(img).flatten())  # Flatten to vector
+                y.append(int(label))
+    return np.array(X), np.array(y)
