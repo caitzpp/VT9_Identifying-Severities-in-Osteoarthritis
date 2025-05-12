b6869865992a2fc4fb69369ca654c196594f15e9
Caitlyn Zuppinger <caitlyn.zuppinger@swisscom.com>
2025-05-12 17:55:44 +0200

    feat(evaluate): fixed it so it includes test data

--- utils/label_eval_utils.py
+++ utils/label_eval_utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+import pandas as pd
 
 def scatter_plot(df, x = 'label', x_title = 'KL Score', y = 'anoms_count', y_title = 'Count Anomalities', label = None, title = None, save_path = None): # y = 'anom_av', y_title = 'Average Anom Score'
     '''
@@ -40,5 +41,54 @@ def pairplot(df, values = ['label', 'anoms_count', 'sim', 'av'], hue = None, tit
     if title is not None:
         plt.title('Box Plot: Distribution of Anoms Count by Label')
     plt.show()
+    if save_path is not None:
+        plt.savefig(save_path)
+
+def plot_comparison_boxplot(df1, df2, column, grouping_column = None,  title = None, log= False, save_path = None):
+    if grouping_column is not None:
+        df1 = df1[[grouping_column, column]].value_counts().reset_index()
+        df2 = df2[[grouping_column, column]].value_counts().reset_index()
+
+    else:
+        df1 = df1[column].value_counts().reset_index()
+        df2 = df2[column].value_counts().reset_index()
+
+    df1["test_train"] = "Train"
+    df2["test_train"] = "Test"
+
+    if log == True:
+        y_col = "log_count"
+    else:
+        y_col = "count"
+    
+    if grouping_column is not None:
+        comp_df = pd.concat([df1[[grouping_column, column, 'test_train', "count"]], df2[[grouping_column, column, 'test_train', "count"]]])
+        comp_df = comp_df.sort_values(by=column)
+        if log == True:
+            comp_df[y_col] = np.log(comp_df['count'])
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=column, y = y_col, hue=grouping_column, data=comp_df)
+        if title is not None:
+            plt.title(title)
+        plt.xlabel('Value')
+        plt.ylabel(y_col)
+        plt.legend(title=grouping_column)
+        plt.show()
+    else:
+        comp_df = pd.concat([df1[[column, 'test_train', "count"]], df2[[column, 'test_train', "count"]]])
+        comp_df = comp_df.sort_values(by=column)
+        if log == True:
+            comp_df[y_col] = np.log(comp_df['count'])
+            
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=column, y = y_col, hue="test_train", data=comp_df)
+        if title is not None:
+            plt.title(title)
+        plt.xlabel('Value')
+        plt.ylabel(y_col)
+        plt.legend(title="test_train")
+        plt.show()
+    
     if save_path is not None:
         plt.savefig(save_path)
