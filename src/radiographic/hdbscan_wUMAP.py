9d4731b2585f297bace5bec7dda95f9e4b2984a3
caitzpp <caitlyn.zuppinger@outlook.com>
2025-08-12 17:39:26 +0200

    feat(hdbscan): tried to remake results, to also save UMAP, but wasn't succesful, therefore for now going to save it like this.

---
+++ src/radiographic/hdbscan_wUMAP.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import os
+import config
+import wandb
+
+from umap import UMAP
+from sklearn.cluster import HDBSCAN
+
+from utils.load_utils import load_image_folder_as_array, load_npy_folder_as_array, get_next_run_folder
+from utils.hdbscan_utils import save_results
+import datetime
+
+
+today = datetime.date.today()
+
+
+
+DATA_PATH = config.FEATURE_PATH
+run = "run150"
+SAVE_PATH = os.path.join(config.OUTPUT_PATH, "symptomatic","aggr",  run)
+os.makedirs(SAVE_PATH, exist_ok=True)
+
+#TODO: get kl file and kl filepath
+random_state=42
+
+umap_keys = ['n_neighbors', 'min_dist', 'n_components', 'metric']
+hdbscan_keys = ['min_cluster_size', 'min_samples', 'cluster_selection_method', 
+                'metric', 'metric_params', 'max_cluster_size',
+                'cluster_selection_epsilon', 'algorithm', 'leaf_size',
+                'store_centers', 'alpha']
+
+umap_defaults = {
+    'n_neighbors': 15,
+    'min_dist': 0.1,
+    'n_components': 2,
+    'metric': 'euclidean',
+    'random_state': random_state
+}
+
+hdbscan_defaults = {
+    'min_cluster_size': 10,
+    'min_samples': None,
+    'cluster_selection_method': 'eom',
+    'metric': 'euclidean',
+    'metric_params': None,
+    'max_cluster_size': None,
+    'cluster_selection_epsilon': 0.0,
+    'algorithm': 'auto',
+    'leaf_size': 40,
+    'store_centers': 'centroid',
+    'alpha': 1.0
+}
+
+
+if __name__=="__main__":
+    run_folder_name, run_path = get_next_run_folder(save_dir)
+
+    run = wandb.init(
+        name = f"{os.path.basename(save_dir)}_{run_folder_name}"
+        )
+    wandb_config = wandb.config
+
+    STAGE = wandb_config.get('stage', 'ss')
+    NEPOCH = wandb_config.get('n_epochs', 400)
+    MODEL_NAME = wandb_config.get('model_name', 'mod_st')
+    seed = wandb_config.get('seed', '34')
+    on_test_set = wandb_config.get('on_test_set', False)
+    DATA_TYPE = wandb_config.get('data_type', 'features')
+
+    files = os.listdir(os.path.join(DATA_PATH, STAGE))
+
+    file = [f for f in files if (MODEL_NAME in f) & (NEPOCH in f) & (seed in f) & ('on_test_set' not in f)]
+
+    if len(file) == 1:
+        folder_dir = file[0]
+        feature_dir = os.path.join(DATA_PATH, STAGE, folder_dir)
+
+        if on_test_set:
+            feature_dir = os.path.join(feature_dir, 'test')
+        else:
+            feature_dir = os.path.join(feature_dir, 'train')
+    else:
+        print("more than one folder found: ", file)
+        raise
+
+    UMAP_PARAMS = {k: wandb_config.get(f'umap.{k}', umap_defaults[k]) for k in umap_keys}
+    HDBSCAN_PARAMS = {k: wandb_config.get(f'hdbscan.{k}', hdbscan_defaults[k]) for k in hdbscan_keys}
+
+    if HDBSCAN_PARAMS['min_samples'] == -1:
+        HDBSCAN_PARAMS['min_samples'] = None
+
+    #TODO: get correct y values, so get labels such as KL for later
+
+    X, y, _ = load_npy_folder_as_array(feature_dir)
+    X_umap = UMAP(**UMAP_PARAMS).fit_transform(X)
+    labels = hdbscan.HDBSCAN(**HDBSCAN_PARAMS).fit_predict(X_umap)
+
+    save_folder = run_folder_name
+    filename = f"{run.name}_umap_hdbscan"
+
+    save_dir_temp = os.path.join(save_dir, save_folder)
+    os.makedirs(save_dir_temp, exist_ok=True)
+
+    base_name, results_df = save_results(df2, clusterer, {
+        'umap': UMAP_PARAMS,
+        'hdbscan': HDBSCAN_PARAMS
+    }, scaler, save_dir_temp, filename, use_wandb=True)
+
+
+
+
+    cmap5 = plt.cm.get_cmap('tab10', 5)
+    
+    if UMAP_PARAMS['n_components'] == 2: 
+        fig, ax = plt.subplots(figsize=(12, 10))
+        scatter = ax.scatter(
+            X_umap[:, 0],
+            X_umap[:, 1],
+            c=y,
+            cmap=cmap5,
+            s=30,
+            alpha= 0.7,
+            edgecolor='k',
+            linewidth=0.5,
+        )
+    elif UMAP_PARAMS['n_components'] == 3:
+        fig = plt.figure(figsize=(12, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        scatter = ax.scatter(
+            X_umap[:, 0],
+            X_umap[:, 1],
+            X_umap[:, 2],
+            c=y,
+            cmap=cmap5,
+            s=30,
+            alpha=0.7,
+            edgecolor='k',
+            linewidth=0.5,
+        )
+
+    handles, labels = scatter.legend_elements(
+        prop="colors",
+        num=4
+    )
+
+    ax.legend(
+        handles, labels,
+        title="Label",
+        loc="upper left",
+        bbox_to_anchor=(1.02, 1),
+        borderaxespad=0.,
+        frameon=False
+    )
+
+    ax.set_title("True Labels")
+
+    plt.tight_layout()
+
+    if on_test_set:
+        plt.savefig(os.path.join(SAVE_PATH, MODEL_NAME + '_' + DATA_TYPE + '_' + STAGE + '_' + NEPOCH + '_' + seed + '_' + 'n_neighbors' + '_' + str(UMAP_PARAMS['n_neighbors']) +'_' + 'min_dist' +'_' + str(UMAP_PARAMS['min_dist'])
+                             +'_' + "lr_" + str(UMAP_PARAMS['learning_rate']) + '_' + 'ncomp' + str(UMAP_PARAMS['n_components']) + '_' + str(UMAP_PARAMS['metric']) +'_' + 'testset' +'_comparison.png'))
+    else:
+        plt.savefig(os.path.join(SAVE_PATH, MODEL_NAME + '_' + DATA_TYPE + '_' + STAGE + '_' + NEPOCH + '_' + seed +'_' + 'n_neighbors' +'_' + str(UMAP_PARAMS['n_neighbors']) + '_' +'min_dist' +'_' + str(UMAP_PARAMS['min_dist'])
+                                +'_' +"lr_" + str(UMAP_PARAMS['learning_rate']) + '_' + 'ncomp' + str(UMAP_PARAMS['n_components']) + '_'+ str(UMAP_PARAMS['metric']) +'_comparison.png'))
--- src/symptomatic/hdbscan_wUMAP.py
+++ src/symptomatic/hdbscan_wUMAP.py
@@ -1,158 +1,197 @@
-import matplotlib.pyplot as plt
 import os
-import config
-import wandb
+import datetime
+import pandas as pd
+import numpy as np
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.cluster import HDBSCAN
 from umap import UMAP
-import hdbscan
 
-from utils.load_utils import load_image_folder_as_array, load_npy_folder_as_array, get_next_run_folder
-from utils.hdbscan_utils import save_results
+import config
+from utils.hdbscan_utils import save_results, plot_hdbscan, prep_data, get_metrics_hdbscan, train_fold, save_umap_true_plot
 
-
-
-DATA_PATH = config.FEATURE_PATH
-
-SAVE_PATH = os.path.join(config.OUTPUT_PATH, "UMAP", "img")
-
-#TODO: get kl file and kl filepath
+today = datetime.date.today()
 
 
-umap_keys = ['n_neighbors', 'min_dist', 'n_components', 'metric']
-hdbscan_keys = ['min_cluster_size', 'min_samples', 'cluster_selection_method', 
-                'metric', 'metric_params', 'max_cluster_size',
-                'cluster_selection_epsilon', 'algorithm', 'leaf_size',
-                'store_centers', 'alpha']
 
-umap_defaults = {
+base_dir = config.RAW_DATA_PATH
+proc_dir = config.PROC_DATA_PATH
+
+run = "run150"
+save_dir = os.path.join(proc_dir, "symptomatic","aggr",  run)
+os.makedirs(save_dir, exist_ok=True)
+
+folder = "2025-07-28_data_exploration"
+df_filename = "inmodi_data_personalinformation_kl_woSC.csv"
+# folder = "2025-08-11_data_exploration"
+# df_filename = "inmodi_data_questionnaire_kl_woSC.csv"
+
+if "personalinformation" in df_filename:
+    cols = ['name',
+        #'record_id', # id column
+                #'visit', 'side', 
+                'pain', 
+                'age', 
+                # 'ce_height', 
+                # 'ce_weight',
+            'ce_bmi', 
+            'ce_fm', 
+            'gender', 
+            'OKS_score', 
+            'UCLA_score', 
+            'FJS_score',
+            'KOOS_pain', 
+            'KOOS_symptoms', 
+            'KOOS_sport', 
+            'KOOS_adl', 
+            'KOOS_qol',
+            'KL-Score'
+        ]
+elif "questionnaire" in df_filename:
+    cols = [
+        # 'record_id', 'visit', 'side', 
+        # 'pain', 'age', #'ce_height', 'ce_weight',
+        #    'ce_bmi', 'ce_fm', 
+        'gender',  #keep because I believe it will be relevant
+       'name', 
+       'KL-Score',  
+       'oks_q1', 'oks_q2', 'oks_q3', 'oks_q4',
+       'oks_q5', 'oks_q6', 'oks_q7', 'oks_q8', 'oks_q9', 'oks_q10', 'oks_q11',
+       'oks_q12', 'koos_s1', 
+       'koos_s2', 'koos_s3', 'koos_s4', 'koos_s5', 'koos_s6',
+       'koos_s7', 
+       'koos_p1', 'koos_p2', 'koos_p3', 'koos_p4', 'koos_p5',
+       'koos_p6', 'koos_p7', 'koos_p8', 'koos_p9', 
+       'koos_a1', 'koos_a2',
+       'koos_a3', 'koos_a4', 'koos_a5', 'koos_a6', 'koos_a7', 'koos_a8',
+       'koos_a9', 'koos_a10', 'koos_a11', 'koos_a12', 'koos_a13', 'koos_a14',
+       'koos_a15', 'koos_a16', 'koos_a17', 
+       'koos_sp1', 'koos_sp2', 'koos_sp3',
+       'koos_sp4', 'koos_sp5', 
+       'koos_q1', 'koos_q2', 'koos_q3', 'koos_q4'
+       ]
+
+random_state=42
+replace_NanValues = False
+wUMAP = True
+k=None
+use_wandb = False
+
+umap_params = {
     'n_neighbors': 15,
-    'min_dist': 0.1,
-    'n_components': 2,
-    'metric': 'euclidean'
+    'min_dist': 0.001,
+    'n_components': 3,
+    'metric': 'euclidean',
+    #'random_state': random_state
 }
 
-hdbscan_defaults = {
-    'min_cluster_size': 10,
-    'min_samples': None,
-    'cluster_selection_method': 'eom',
-    'metric': 'euclidean',
-    'metric_params': None,
-    'max_cluster_size': None,
-    'cluster_selection_epsilon': 0.0,
-    'algorithm': 'auto',
-    'leaf_size': 40,
-    'store_centers': 'centroid',
-    'alpha': 1.0
+hdbscan_params = {
+    "min_cluster_size": 10,
+            "min_samples": 5,
+            "cluster_selection_method": "eom",
+            "metric": "euclidean",
+            "cluster_selection_epsilon": 0.0,
+            "algorithm": "auto",
+            "metric_params": None,
+            "max_cluster_size": None,
+            "leaf_size": 40,
+            "store_centers": "centroid",
+            "alpha": 2
 }
 
 
 if __name__=="__main__":
-    run_folder_name, run_path = get_next_run_folder(save_dir)
+    df = pd.read_csv(os.path.join(proc_dir, folder, df_filename))
 
-    run = wandb.init(
-        name = f"{os.path.basename(save_dir)}_{run_folder_name}"
-        )
-    wandb_config = wandb.config
+    df2 = df[cols].copy()
 
-    STAGE = wandb_config.get('stage', 'ss')
-    NEPOCH = wandb_config.get('n_epochs', 400)
-    MODEL_NAME = wandb_config.get('model_name', 'mod_st')
-    seed = wandb_config.get('seed', '34')
-    on_test_set = wandb_config.get('on_test_set', False)
-    DATA_TYPE = wandb_config.get('data_type', 'features')
+    if replace_NanValues==False:
+        print("Dataframe before dropping NaN values: ", df2.shape)
+        df2 = df2.dropna(axis=0, how='any')
 
-    files = os.listdir(os.path.join(DATA_PATH, STAGE))
+        print()
+        print("Dataframe after dropping NaN values: ", df2.shape)
 
-    file = [f for f in files if (MODEL_NAME in f) & (NEPOCH in f) & (seed in f) & ('on_test_set' not in f)]
+    # 'gender' convert to int
+    if "gender" in df2.columns:
+        df2['is_male'] = df2['gender'].apply(lambda x: 1 if x=='male' else 0)
+        df2 = df2.drop(columns= 'gender')
 
-    if len(file) == 1:
-        folder_dir = file[0]
-        feature_dir = os.path.join(DATA_PATH, STAGE, folder_dir)
-
-        if on_test_set:
-            feature_dir = os.path.join(feature_dir, 'test')
-        else:
-            feature_dir = os.path.join(feature_dir, 'train')
-    else:
-        print("more than one folder found: ", file)
-        raise
-
-    UMAP_PARAMS = {k: wandb_config.get(f'umap.{k}', umap_defaults[k]) for k in umap_keys}
-    HDBSCAN_PARAMS = {k: wandb_config.get(f'hdbscan.{k}', hdbscan_defaults[k]) for k in hdbscan_keys}
-
-    if HDBSCAN_PARAMS['min_samples'] == -1:
-        HDBSCAN_PARAMS['min_samples'] = None
-
-    #TODO: get correct y values, so get labels such as KL for later
-
-    X, y, _ = load_npy_folder_as_array(feature_dir)
-    X_umap = UMAP(**UMAP_PARAMS).fit_transform(X)
-    labels = hdbscan.HDBSCAN(**HDBSCAN_PARAMS).fit_predict(X_umap)
-
-    save_folder = run_folder_name
-    filename = f"{run.name}_umap_hdbscan"
-
-    save_dir_temp = os.path.join(save_dir, save_folder)
-    os.makedirs(save_dir_temp, exist_ok=True)
-
-    base_name, results_df = save_results(df2, clusterer, {
-        'umap': UMAP_PARAMS,
-        'hdbscan': HDBSCAN_PARAMS
-    }, scaler, save_dir_temp, filename, use_wandb=True)
-
-
-
-
-    cmap5 = plt.cm.get_cmap('tab10', 5)
+    scaler = StandardScaler()
     
-    if UMAP_PARAMS['n_components'] == 2: 
-        fig, ax = plt.subplots(figsize=(12, 10))
-        scatter = ax.scatter(
-            X_umap[:, 0],
-            X_umap[:, 1],
-            c=y,
-            cmap=cmap5,
-            s=30,
-            alpha= 0.7,
-            edgecolor='k',
-            linewidth=0.5,
-        )
-    elif UMAP_PARAMS['n_components'] == 3:
-        fig = plt.figure(figsize=(12, 10))
-        ax = fig.add_subplot(111, projection='3d')
-        scatter = ax.scatter(
-            X_umap[:, 0],
-            X_umap[:, 1],
-            X_umap[:, 2],
-            c=y,
-            cmap=cmap5,
-            s=30,
-            alpha=0.7,
-            edgecolor='k',
-            linewidth=0.5,
-        )
+    X_umap, y, df2_scaled = prep_data(df2, scaler, umap_params = umap_params, wUMAP=wUMAP, id_col='name', y_value='KL-Score')
 
-    handles, labels = scatter.legend_elements(
-        prop="colors",
-        num=4
-    )
+    filename = f"{today}_umap_hdbscan_scaled"
 
-    ax.legend(
-        handles, labels,
-        title="Label",
-        loc="upper left",
-        bbox_to_anchor=(1.02, 1),
-        borderaxespad=0.,
-        frameon=False
-    )
+    if wUMAP:
+        np.savez_compressed(os.path.join(save_dir, f"{today}_umap_embedding.npz"), X_umap=X_umap)
+        save_umap_true_plot(X_umap, y, out_path = os.path.join(save_dir, f"{today}_umap.png"), 
+                            umap_params=umap_params)
+    # save_dir_temp = os.path.join(save_dir, save_folder)
+    # os.makedirs(save_dir_temp, exist_ok=True)
 
-    ax.set_title("True Labels")
+    if k is None:
+        print("\n--- No cross-validation, training on full dataset ---")
+        clusterer = HDBSCAN(**hdbscan_params).fit(X_umap)
 
-    plt.tight_layout()
+        ch_score = calinski_harabasz_score(X_umap, clusterer.labels_)
 
-    if on_test_set:
-        plt.savefig(os.path.join(SAVE_PATH, MODEL_NAME + '_' + DATA_TYPE + '_' + STAGE + '_' + NEPOCH + '_' + seed + '_' + 'n_neighbors' + '_' + str(UMAP_PARAMS['n_neighbors']) +'_' + 'min_dist' +'_' + str(UMAP_PARAMS['min_dist'])
-                             +'_' + "lr_" + str(UMAP_PARAMS['learning_rate']) + '_' + 'ncomp' + str(UMAP_PARAMS['n_components']) + '_' + str(UMAP_PARAMS['metric']) +'_' + 'testset' +'_comparison.png'))
-    else:
-        plt.savefig(os.path.join(SAVE_PATH, MODEL_NAME + '_' + DATA_TYPE + '_' + STAGE + '_' + NEPOCH + '_' + seed +'_' + 'n_neighbors' +'_' + str(UMAP_PARAMS['n_neighbors']) + '_' +'min_dist' +'_' + str(UMAP_PARAMS['min_dist'])
-                                +'_' +"lr_" + str(UMAP_PARAMS['learning_rate']) + '_' + 'ncomp' + str(UMAP_PARAMS['n_components']) + '_'+ str(UMAP_PARAMS['metric']) +'_comparison.png'))
+        base_name, results_df = save_results(df=df2_scaled, clusterer=clusterer, params={
+            'umap': umap_params,
+            'hdbscan': hdbscan_params
+        }, scaler=scaler, save_dir=save_dir, filename=filename, id='name', use_wandb=use_wandb)
+    
+        get_metrics_hdbscan(results_df, df, save_dir, base_name, score='cluster_label', label='KL-Score', use_wandb=use_wandb)
+
+        # Plot the results
+        plot_hdbscan(X_umap, clusterer.labels_,
+                    probabilities=clusterer.probabilities_,
+                    save_path=os.path.join(save_dir, f"{base_name}_plot.png"))
+    elif k is not None:
+        kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=random_state) if k is not None else None
+
+
+        print("\n--- Cross-validation ---")
+
+        l_noise_count = []
+        l_avg_probs = []
+        l_entropy = []
+        l_normalized_entropy = []
+        l_spearman_correlation = []
+        l_auc = []
+        l_auc_mid = []
+        l_auc_mid2 = []
+        l_auc_sev = []
+        l_nmi = []
+        l_chscore = []
+        for fold, (train_idx, test_idx) in enumerate(kf.split(X_umap, y)):
+            print(f"\n--- Fold {fold+1} ---")
+            base_name, results_df, clusterer, X_train, df_train, _, _, ch_score = train_fold(fold=fold, 
+                                                                           train_idx=train_idx, 
+                                                                           test_idx=test_idx, 
+                                                                           X=X_umap, y=y, 
+                                                                           df=df2_scaled,
+                                                                           hdbscan_params=hdbscan_params,
+                                                                           umap_params=umap_params,
+                                                                           scaler=scaler,
+                                                                           filename=filename,
+                                                                           save_dir_temp=save_dir)
+            noise_count, avg_probs, membership_entropy, normalized_entropy, spr, auc, auc_mid, auc_mid2, auc_sev, nmi = get_metrics_hdbscan(results_df, df, save_dir, base_name, score='cluster_label', label='KL-Score', use_wandb=use_wandb, fold=fold)
+            l_noise_count.append(noise_count)
+            mean_avg_probs = avg_probs.mean()
+            l_avg_probs.append(mean_avg_probs)
+            l_entropy.append(membership_entropy)
+            l_normalized_entropy.append(normalized_entropy)
+            l_spearman_correlation.append(spr)
+            l_auc.append(auc)
+            l_auc_mid.append(auc_mid)
+            l_auc_mid2.append(auc_mid2)
+            l_auc_sev.append(auc_sev)
+            l_nmi.append(nmi)
+            l_chscore.append(ch_score)
+
+            plot_hdbscan(X_train, clusterer.labels_,
+                    probabilities=clusterer.probabilities_,
+                    save_path=os.path.join(save_dir, f"{base_name}__{fold}_plot.png"))
