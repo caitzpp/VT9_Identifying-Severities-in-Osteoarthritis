1f186afb018fc901fcfa1498177c9f15aeae5e94
caitzpp <caitlyn.zuppinger@outlook.com>
2025-08-12 09:34:43 +0200

    feat(sweep_whole_questionnaire): adapted yaml and tuning file so that it supports the whole questionnaire data as well,although at the moment we are focussing on KOOS and OKS, since these had high spearman correlation (negative) and also appeared to give us the least issues with null values.

--- src/symptomatic/hdbscan_clustering_tuning.py
+++ src/symptomatic/hdbscan_clustering_tuning.py
@@ -36,35 +36,59 @@ stratify_on = 'KL-Score'
 if folder is not None:
     save_dir = os.path.join(proc_dir, folder)
 else:
-    save_dir = os.path.join(proc_dir, f"{today}_hdbscan")
+    save_dir = os.path.join(proc_dir, f"{today}_hdbscan", 'questionnaire')
 os.makedirs(save_dir, exist_ok=True)
 
-folder = "2025-07-28_data_exploration"
-df_filename = "inmodi_data_personalinformation_kl_woSC.csv"
+# folder = "2025-07-28_data_exploration"
+# df_filename = "inmodi_data_personalinformation_kl_woSC.csv"
+folder = "2025-08-11_data_exploration"
+df_filename = "inmodi_data_questionnaire_kl_woSC.csv"
 # kl_file = "inmodi_data_personalinformation_kl.csv"
 # kl_filepath = os.path.join(proc_dir, folder, kl_file)
 
 #Choose relevant columns
-cols = ['name',
-    #'record_id', # id column
-            #'visit', 'side', 
-            'pain', 
-            'age', 
-            # 'ce_height', 
-            # 'ce_weight',
-        'ce_bmi', 
-        'ce_fm', 
-        'gender', 
-        'OKS_score', 
-        'UCLA_score', 
-        'FJS_score',
-        'KOOS_pain', 
-        'KOOS_symptoms', 
-        'KOOS_sport', 
-        'KOOS_adl', 
-        'KOOS_qol',
-        'KL-Score'
-    ]
+# cols = ['name',
+#     #'record_id', # id column
+#             #'visit', 'side', 
+#             'pain', 
+#             'age', 
+#             # 'ce_height', 
+#             # 'ce_weight',
+#         'ce_bmi', 
+#         'ce_fm', 
+#         'gender', 
+#         'OKS_score', 
+#         'UCLA_score', 
+#         'FJS_score',
+#         'KOOS_pain', 
+#         'KOOS_symptoms', 
+#         'KOOS_sport', 
+#         'KOOS_adl', 
+#         'KOOS_qol',
+#         'KL-Score'
+#     ]
+cols = [
+    # 'record_id', 'visit', 'side', 
+    # 'pain', 'age', #'ce_height', 'ce_weight',
+    #    'ce_bmi', 'ce_fm', 
+        'gender',  #keep because I believe it will be relevant
+       'name', 
+       'KL-Score',  
+       'oks_q1', 'oks_q2', 'oks_q3', 'oks_q4',
+       'oks_q5', 'oks_q6', 'oks_q7', 'oks_q8', 'oks_q9', 'oks_q10', 'oks_q11',
+       'oks_q12', 'koos_s1', 
+       'koos_s2', 'koos_s3', 'koos_s4', 'koos_s5', 'koos_s6',
+       'koos_s7', 
+       'koos_p1', 'koos_p2', 'koos_p3', 'koos_p4', 'koos_p5',
+       'koos_p6', 'koos_p7', 'koos_p8', 'koos_p9', 
+       'koos_a1', 'koos_a2',
+       'koos_a3', 'koos_a4', 'koos_a5', 'koos_a6', 'koos_a7', 'koos_a8',
+       'koos_a9', 'koos_a10', 'koos_a11', 'koos_a12', 'koos_a13', 'koos_a14',
+       'koos_a15', 'koos_a16', 'koos_a17', 
+       'koos_sp1', 'koos_sp2', 'koos_sp3',
+       'koos_sp4', 'koos_sp5', 
+       'koos_q1', 'koos_q2', 'koos_q3', 'koos_q4'
+       ]
 
 wandb.login(key=config.HDBSCAN_SYMP_WANDBAPI_KEY)
 
