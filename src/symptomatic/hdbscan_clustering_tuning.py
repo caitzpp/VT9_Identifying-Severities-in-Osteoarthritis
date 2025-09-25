5de86cf4fd94eb385d5fb18b31147b1e72916293
caitzpp <caitlyn.zuppinger@outlook.com>
2025-09-25 12:52:53 +0200

    feat(clusteringpipeline)

--- src/symptomatic/hdbscan_clustering_tuning.py
+++ src/symptomatic/hdbscan_clustering_tuning.py
@@ -113,138 +113,137 @@ if __name__ == "__main__":
         , config = config_w
         )
     wandb_config = run.config
-    print(wandb_config)
-    wandb.finish()
-    # wUMAP = wandb_config.get('wUMAP', True)
-    # replace_NanValues = wandb_config.get('replace_NanValues', False)
-    # k = wandb_config.get('k-cv', None)
+  
+    wUMAP = wandb_config.get('wUMAP', True)
+    replace_NanValues = wandb_config.get('replace_NanValues', False)
+    k = wandb_config.get('k-cv', None)
 
-    # if wUMAP:
-    #     umap_params = {k: wandb_config.get(f'umap.{k}', umap_defaults[k]) for k in umap_keys}
-    # else:
-    #     umap_params = "woUMAP"
-    # hdbscan_params = {k: wandb_config.get(f'hdbscan.{k}', hdbscan_defaults[k]) for k in hdbscan_keys}
+    if wUMAP:
+        umap_params = {k: wandb_config.get(f'umap.{k}', umap_defaults[k]) for k in umap_keys}
+    else:
+        umap_params = "woUMAP"
+    hdbscan_params = {k: wandb_config.get(f'hdbscan.{k}', hdbscan_defaults[k]) for k in hdbscan_keys}
 
-    # if hdbscan_params['min_samples'] == -1:
-    #     hdbscan_params['min_samples'] = None
+    if hdbscan_params['min_samples'] == -1:
+        hdbscan_params['min_samples'] = None
     
-    # df = pd.read_csv(os.path.join(proc_dir, folder, df_filename))
+    df = pd.read_csv(os.path.join(proc_dir, folder, df_filename))
 
-    # df2 = df[cols].copy()
+    df2 = df[cols].copy()
 
-    # if replace_NanValues==False:
-    #     print("Dataframe before dropping NaN values: ", df2.shape)
-    #     df2 = df2.dropna(axis=0, how='any')
+    if replace_NanValues==False:
+        print("Dataframe before dropping NaN values: ", df2.shape)
+        df2 = df2.dropna(axis=0, how='any')
 
-    #     print()
-    #     print("Dataframe after dropping NaN values: ", df2.shape)
+        print()
+        print("Dataframe after dropping NaN values: ", df2.shape)
 
-    # # 'gender' convert to int
-    # df2['is_male'] = df['gender'].apply(lambda x: 1 if x=='male' else 0)
-    # df2 = df2.drop(columns= 'gender')
-    # save_folder = run_folder_name
-    # filename = f"{run.name}_umap_hdbscan_scaled"
+    # 'gender' convert to int
+    df2['is_male'] = df['gender'].apply(lambda x: 1 if x=='male' else 0)
+    df2 = df2.drop(columns= 'gender')
+    save_folder = run_folder_name
+    filename = f"{run.name}_umap_hdbscan_scaled"
 
-    # save_dir_temp = os.path.join(save_dir, save_folder)
-    # os.makedirs(save_dir_temp, exist_ok=True)
+    save_dir_temp = os.path.join(save_dir, save_folder)
+    os.makedirs(save_dir_temp, exist_ok=True)
 
-    # scaler = StandardScaler()
+    scaler = StandardScaler()
     
-    # X_umap, y, df2_scaled, artifacts = prep_data(df2, scaler, umap_params = umap_params, wUMAP=wUMAP, id_col='name', y_value='KL-Score', save_path=save_dir_temp)
+    X_umap, y, df2_scaled, artifacts = prep_data(df2, scaler, umap_params = umap_params, wUMAP=wUMAP, id_col='name', y_value='KL-Score', save_path=save_dir_temp)
 
-    # kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=random_state) if k is not None else None
+    kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=random_state) if k is not None else None
 
-    # if kf is None:
-    #     print("\n--- No cross-validation, training on full dataset ---")
-    #     clusterer = HDBSCAN(**hdbscan_params).fit(X_umap)
-    #     clusterer_path = os.path.join(save_dir_temp, f"{filename}_clusterer.pkl")
-    #     joblib.dump(clusterer, clusterer_path)
-    #     artifacts['clusterer'] = clusterer_path
+    if kf is None:
+        print("\n--- No cross-validation, training on full dataset ---")
+        clusterer = HDBSCAN(**hdbscan_params).fit(X_umap)
+        clusterer_path = os.path.join(save_dir_temp, f"{filename}_clusterer.pkl")
+        joblib.dump(clusterer, clusterer_path)
+        artifacts['clusterer'] = clusterer_path
 
-    #     ch_score = calinski_harabasz_score(X_umap, clusterer.labels_)
+        ch_score = calinski_harabasz_score(X_umap, clusterer.labels_)
 
-    #     wandb.log(
-    #         {'calinski_harabasz_score': ch_score,
-    #          'calinski_harabasz_scorev2': int(ch_score)}
-    #     )
+        wandb.log(
+            {'calinski_harabasz_score': ch_score,
+             'calinski_harabasz_scorev2': int(ch_score)}
+        )
 
-    #     base_name, results_df = save_results(df=df2_scaled, clusterer=clusterer, params={
-    #         'umap': umap_params,
-    #         'hdbscan': hdbscan_params
-    #     }, scaler=scaler, save_dir=save_dir_temp, artifacts = artifacts, filename=filename, id='name', use_wandb=True)
+        base_name, results_df = save_results(df=df2_scaled, clusterer=clusterer, params={
+            'umap': umap_params,
+            'hdbscan': hdbscan_params
+        }, scaler=scaler, save_dir=save_dir_temp, artifacts = artifacts, filename=filename, id='name', use_wandb=True)
     
-    #     get_metrics_hdbscan(results_df, df, save_dir_temp, base_name, score='cluster_label', label='KL-Score', use_wandb=True)
+        get_metrics_hdbscan(results_df, df, save_dir_temp, base_name, score='cluster_label', label='KL-Score', use_wandb=True)
 
-    #     # Plot the results
-    #     plot_hdbscan(X_umap, clusterer.labels_,
-    #                 probabilities=clusterer.probabilities_,
-    #                 save_path=os.path.join(save_dir_temp, f"{base_name}_plot.png"))
-    # elif kf is not None:
-    #     print("\n--- Cross-validation ---")
+        # Plot the results
+        plot_hdbscan(X_umap, clusterer.labels_,
+                    probabilities=clusterer.probabilities_,
+                    save_path=os.path.join(save_dir_temp, f"{base_name}_plot.png"))
+    elif kf is not None:
+        print("\n--- Cross-validation ---")
 
-    #     l_noise_count = []
-    #     l_avg_probs = []
-    #     l_entropy = []
-    #     l_normalized_entropy = []
-    #     l_spearman_correlation = []
-    #     l_auc = []
-    #     l_auc_mid = []
-    #     l_auc_mid2 = []
-    #     l_auc_sev = []
-    #     l_nmi = []
-    #     l_chscore = []
-    #     for fold, (train_idx, test_idx) in enumerate(kf.split(X_umap, y)):
-    #         print(f"\n--- Fold {fold+1} ---")
-    #         base_name, results_df, clusterer, X_train, df_train, _, _, ch_score = train_fold(fold=fold, 
-    #                                                                        train_idx=train_idx, 
-    #                                                                        test_idx=test_idx, 
-    #                                                                        X=X_umap, y=y, 
-    #                                                                        df=df2_scaled,
-    #                                                                        hdbscan_params=hdbscan_params,
-    #                                                                        umap_params=umap_params,
-    #                                                                        scaler=scaler,
-    #                                                                        filename=filename,
-    #                                                                        save_dir_temp=save_dir_temp)
-    #         noise_count, avg_probs, membership_entropy, normalized_entropy, spr, auc, auc_mid, auc_mid2, auc_sev, nmi = get_metrics_hdbscan(results_df, df, save_dir_temp, base_name, score='cluster_label', label='KL-Score', use_wandb=True, fold=fold)
-    #         l_noise_count.append(noise_count)
-    #         mean_avg_probs = avg_probs.mean()
-    #         l_avg_probs.append(mean_avg_probs)
-    #         l_entropy.append(membership_entropy)
-    #         l_normalized_entropy.append(normalized_entropy)
-    #         l_spearman_correlation.append(spr)
-    #         l_auc.append(auc)
-    #         l_auc_mid.append(auc_mid)
-    #         l_auc_mid2.append(auc_mid2)
-    #         l_auc_sev.append(auc_sev)
-    #         l_nmi.append(nmi)
-    #         l_chscore.append(ch_score)
+        l_noise_count = []
+        l_avg_probs = []
+        l_entropy = []
+        l_normalized_entropy = []
+        l_spearman_correlation = []
+        l_auc = []
+        l_auc_mid = []
+        l_auc_mid2 = []
+        l_auc_sev = []
+        l_nmi = []
+        l_chscore = []
+        for fold, (train_idx, test_idx) in enumerate(kf.split(X_umap, y)):
+            print(f"\n--- Fold {fold+1} ---")
+            base_name, results_df, clusterer, X_train, df_train, _, _, ch_score = train_fold(fold=fold, 
+                                                                           train_idx=train_idx, 
+                                                                           test_idx=test_idx, 
+                                                                           X=X_umap, y=y, 
+                                                                           df=df2_scaled,
+                                                                           hdbscan_params=hdbscan_params,
+                                                                           umap_params=umap_params,
+                                                                           scaler=scaler,
+                                                                           filename=filename,
+                                                                           save_dir_temp=save_dir_temp)
+            noise_count, avg_probs, membership_entropy, normalized_entropy, spr, auc, auc_mid, auc_mid2, auc_sev, nmi = get_metrics_hdbscan(results_df, df, save_dir_temp, base_name, score='cluster_label', label='KL-Score', use_wandb=True, fold=fold)
+            l_noise_count.append(noise_count)
+            mean_avg_probs = avg_probs.mean()
+            l_avg_probs.append(mean_avg_probs)
+            l_entropy.append(membership_entropy)
+            l_normalized_entropy.append(normalized_entropy)
+            l_spearman_correlation.append(spr)
+            l_auc.append(auc)
+            l_auc_mid.append(auc_mid)
+            l_auc_mid2.append(auc_mid2)
+            l_auc_sev.append(auc_sev)
+            l_nmi.append(nmi)
+            l_chscore.append(ch_score)
 
-    #         plot_hdbscan(X_train, clusterer.labels_,
-    #                 probabilities=clusterer.probabilities_,
-    #                 save_path=os.path.join(save_dir_temp, f"{base_name}__{fold}_plot.png"))
+            plot_hdbscan(X_train, clusterer.labels_,
+                    probabilities=clusterer.probabilities_,
+                    save_path=os.path.join(save_dir_temp, f"{base_name}__{fold}_plot.png"))
         
-    #     wandb.log({
-    #         "average_noise_count": np.mean(l_noise_count),
-    #         "average_avg_probs": np.mean(l_avg_probs),
-    #         "average_entropy": np.mean(l_entropy),
-    #         "average_normalized_entropy": np.mean(l_normalized_entropy),
-    #         "average_spearman_correlation": np.mean(l_spearman_correlation),
-    #         "average_auc": np.mean(l_auc),
-    #         "average_auc_mid": np.mean(l_auc_mid),
-    #         "average_auc_mid2": np.mean(l_auc_mid2),
-    #         "average_auc_sev": np.mean(l_auc_sev),
-    #         "average_nmi": np.mean(l_nmi),
-    #         "std_noise_count": np.std(l_noise_count),
-    #         "std_avg_probs": np.std(l_avg_probs),
-    #         "std_entropy": np.std(l_entropy),
-    #         "std_normalized_entropy": np.std(l_normalized_entropy),
-    #         "std_spearman_correlation": np.std(l_spearman_correlation),
-    #         "std_auc": np.std(l_auc),
-    #         "std_auc_mid": np.std(l_auc_mid),
-    #         "std_auc_mid2": np.std(l_auc_mid2),
-    #         "std_auc_sev": np.std(l_auc_sev),
-    #         "std_nmi": np.std(l_nmi),
-    #         "calinski_harabasz_score": ch_score
-    #     })
+        wandb.log({
+            "average_noise_count": np.mean(l_noise_count),
+            "average_avg_probs": np.mean(l_avg_probs),
+            "average_entropy": np.mean(l_entropy),
+            "average_normalized_entropy": np.mean(l_normalized_entropy),
+            "average_spearman_correlation": np.mean(l_spearman_correlation),
+            "average_auc": np.mean(l_auc),
+            "average_auc_mid": np.mean(l_auc_mid),
+            "average_auc_mid2": np.mean(l_auc_mid2),
+            "average_auc_sev": np.mean(l_auc_sev),
+            "average_nmi": np.mean(l_nmi),
+            "std_noise_count": np.std(l_noise_count),
+            "std_avg_probs": np.std(l_avg_probs),
+            "std_entropy": np.std(l_entropy),
+            "std_normalized_entropy": np.std(l_normalized_entropy),
+            "std_spearman_correlation": np.std(l_spearman_correlation),
+            "std_auc": np.std(l_auc),
+            "std_auc_mid": np.std(l_auc_mid),
+            "std_auc_mid2": np.std(l_auc_mid2),
+            "std_auc_sev": np.std(l_auc_sev),
+            "std_nmi": np.std(l_nmi),
+            "calinski_harabasz_score": ch_score
+        })
     
-    # wandb.finish()
+    wandb.finish()
